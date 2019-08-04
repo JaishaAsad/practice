5f8b3d08343ab40a89d8e0bbd81af4f6d333c490
JaishaAsad <50081520+JaishaAsad@users.noreply.github.com>
2019-08-04 17:46:34 +0500

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,3 @@
+x=input("please enter value of radius = ")
+area=3.142*float (x)*float (x)
+print ("the area of the circle ="+str (area)) 
---
+++ q2.py
@@ -0,0 +1,7 @@
+number= int (input("Please enter number to check ="))
+if number > 0:
+    print ("provided number" + str(number) + "is positive")
+elif number < 0:
+    print ("provided number" + str(number) + "is negative")
+else:
+    print ("provided number is zero")   
